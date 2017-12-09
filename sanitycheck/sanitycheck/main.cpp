39997212272ba69af5b47b0e9fbcb5c60252ae78
galliotgreg <galliotgregpro@gmail.com>
2017-12-09 12:19:21 +0100

    Add Point Lights

--- sanitycheck/sanitycheck/main.cpp
+++ sanitycheck/sanitycheck/main.cpp
@@ -31,6 +31,9 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+// lighting
+glm::vec3 lightPos = glm::vec3(1.2f, 1.0f,2.0f);
+
 int main()
 {
     // glfw: initialize and configure
@@ -196,13 +199,19 @@ int main()
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        //lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setVec3("light.position", lightPos.x, lightPos.y, lightPos.z);
         lightingShader.setVec3("viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
 
         // light properties
         lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        // light attenuation properties
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
 
         // material properties
         lightingShader.setFloat("material.shininess", 32.0f);
@@ -243,17 +252,17 @@ int main()
         }
 
 
-        // a lamp object is weird when we only have a directional light, don't render the light object
-        // lampShader.use();
-        // lampShader.setMat4("projection", projection);
-        // lampShader.setMat4("view", view);
-        // model = glm::mat4();
-        // model = glm::translate(model, lightPos);
-        // model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        // lampShader.setMat4("model", model);
+        // a lamp object
+         lampShader.use();
+         lampShader.setMat4("projection", projection);
+         lampShader.setMat4("view", view);
+         model = glm::mat4();
+         model = glm::translate(model, lightPos);
+         model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+         lampShader.setMat4("model", model);
 
-        // glBindVertexArray(lightVAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 36);
+         glBindVertexArray(lightVAO);
+         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
